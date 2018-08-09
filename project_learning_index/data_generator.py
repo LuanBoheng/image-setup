ac500c584fdde2cc7333d3668cdcc2c406fb7ade
LuanBoheng <314887027@qq.com>
2018-08-09 12:50:17 +1000

    install dropbox

--- project_learning_index/data_generator.py
+++ project_learning_index/data_generator.py
@@ -1,8 +1,13 @@
+import random
+
 size = 10
 data = []
-number_generator = random()
+number_generator = random.random
 
 for i in range(10):
-    data.append (number_generator)
+    data.append (number_generator())
+
+data.sort()
 
 print(data)
+
