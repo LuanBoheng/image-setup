ce6a9958be0fba2319a763adea2575f541c4e985
bluan <bluan@student.unimelb.edu.au>
2018-08-14 13:56:17 +1000

    sss

--- add_command.sh
+++ add_command.sh
@@ -3,5 +3,5 @@ echo "ZSH_THEME=\"junkfood\"">> /root/.zshrc
 echo "alias hh=\"history | grep \"">> /root/.zshrc
 echo "alias pp=\"ps -A | grep \"">> /root/.zshrc
 echo "alias gs=\"git status\"">> /root/.zshrc
-echo "alias hh=\"git commit -m 'fast commit'\"">> /root/.zshrc
+echo "alias gm=\"git commit -m 'fast commit'\"">> /root/.zshrc
 source /root/.zshrc
