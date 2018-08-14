a5e1484f83e19501735bd3d8b9b7a022f5b92b74
LuanBoheng <314887027@qq.com>
2018-08-14 15:17:45 +1000

    fast commit

--- install_docker.sh
+++ install_docker.sh
@@ -1,5 +1,5 @@
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-ke    y add - 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com    /linux/ubuntu $(lsb_release -cs) stable" 
-sudo apt-get update 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" 
+apt-get update 
 apt-cache policy docker-ce
-sudo apt-get install -y docker-ce
+apt-get install -y docker-ce
