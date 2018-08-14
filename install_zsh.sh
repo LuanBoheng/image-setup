b5dc4f206b699f275a8e299e5182aa823edcd999
LuanBoheng <314887027@qq.com>
2018-08-14 06:56:40 +0000

    fast commit

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,3 +1,4 @@
 apt install zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+apt install wget
+bash -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 apt install tmux
