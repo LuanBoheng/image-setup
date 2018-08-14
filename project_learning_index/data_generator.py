799c9263ca5638c23df0760caaf4f627c061bc70
LuanBoheng <314887027@qq.com>
2018-08-14 16:17:51 +1000

    fast commit

--- project_learning_index/data_generator.py
+++ project_learning_index/data_generator.py
@@ -1,13 +1,23 @@
+#generate data
+
 import random
 
 size = 10
 data = []
 number_generator = random.random
 
-for i in range(10):
-    data.append (number_generator())
+for i in range(size):
+    data.append ([i,number_generator()])
 
 data.sort()
 
 print(data)
 
+
+#write into file
+import csv
+file_name = "data.txt"
+with open(file_name, 'wb') as f:
+    writer = csv.writer(f)
+    for key in range(len(data)):
+        writer.writerows(data[key])
