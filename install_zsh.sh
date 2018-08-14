6152bfd661a3deaf2c967d61439e3b933044c2f9
bluan <bluan@student.unimelb.edu.au>
2018-08-14 13:52:59 +1000

    fast commit

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,2 +1,2 @@
 apt install zsh
-sh -c "$(wget https://github.com/LuanBoheng/oh-my-zsh/master/tools/install.sh -O -)"
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
